751d6a3cb5d9d04a97486860e16d197a34042ba5
kristjan-steinfeldt <98741231+kristjan-steinfeldt@users.noreply.github.com>
2022-03-28 23:52:11 +0300

    gh

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
+import {useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("dummyUser")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <h1>User: {name}</h1>
+    <div className="container">
+      <div className="textcon"></div>
+      <p className="chatbox">
+        <strong>name:</strong>
+        <span>chat message</span>
+      </p>
+
     </div>
-  );
+  </div>
+
+
+);
 }
 
 export default App;
