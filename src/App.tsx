e623f45084fe37da33cea1e205e0a7074e227d9c
kristjan-steinfeldt <98741231+kristjan-steinfeldt@users.noreply.github.com>
2022-03-29 19:18:50 +0300

    gh

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,65 @@ import {useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  const [name, setName] = useState("dummyUser")
-  return (
-  <div>
-    <h1>User: {name}</h1>
-    <div className="container">
-      <div className="textcon"></div>
-      <p className="chatbox">
-        <strong>name:</strong>
-        <span>chat message</span>
-      </p>
 
-    </div>
-  </div>
+    function countChars(obj :any){
+        if (obj.value.length<0)
+        { // @ts-ignore
+            document.getElementById("inputmsg").innerHTML = obj.value.length+' /100';
+        }
 
+    }
 
-);
+    const [name, setName] = useState("dummyUser")
+    const [chats, setChats] = useState([
+        {name: 'user1', message: 'message1'},
+        {name: 'dummyUser', message: 'message2'},
+    ]);
+
+    const [msg,setMsg] = useState('')
+
+    const sendChat = () => {
+        const c = [...chats];
+        c.push({name,message:msg});
+        setChats(c);
+        setMsg('')
+    }
+
+    return <div>
+        <h1>VOCO</h1>
+        <div className="container">
+            <div id="box" className="box">
+            {chats.map(c => <div className="row">
+                    <div className="container">
+                <div className="textcon">
+                    <p className="chatbox">
+                        <strong>{c.name}:</strong>
+                        <span>{c.message}</span>
+                    </p>
+                </div>
+                    </div>
+                </div>)}
+            </div>
+            <div className="insertbox">
+                <div className="row">
+                    <div className="col-2">
+                        <div className="user text-center"><b>Guest</b></div>
+                    </div>
+                    <div className="col-10">
+                <input className="inmsg" onInput={e=>setMsg(e.currentTarget.value)} value={msg}
+                    placeholder="enter your chat"
+                ></input>
+                        <p id="inputmsg">0/100</p>
+                    </div>
+                </div>
+            </div>
+            <div className="text-right">
+            <button className="btn btn-outline-primary " onClick={(e) => sendChat()}>Send</button>
+            </div>
+        </div>
+        </div>;
 }
 
-export default App;
+            export default App;
